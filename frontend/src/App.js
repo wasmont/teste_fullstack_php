4a570c668f3385bce4f19c7ebbe3c43a5501052a
washington.monteiro <washington.monteiro@executiva.com.br>
2023-07-15 16:52:04 -0300

    Implementação da Listagem dos Registros Produtos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
 import React from 'react';
 import Modal from './components/modal/ModalCadastro.js';
-import AlertDialog from './components/AlertDialog/AlertDialogComponent.js';
-import {Box,Divider,Text,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from '@chakra-ui/react'
+import {Box,Divider,Text,TableContainer} from '@chakra-ui/react'
+import { GetRequestProduto } from './components/requests/GetRequestProduto.js';
 
 function App() {
 
@@ -30,62 +20,14 @@ function App() {
             </div>
             <Divider bg='tomato' orientation='horizontal'/>
             <TableContainer bg='#A0AEC0'>
-              <Table variant='striped' colorScheme='teal' size="md">
-                <TableCaption>Produtos de Eletrodomésticos - @author: Washington Monteiro/2023</TableCaption>
-                <Thead>
-                  <Tr>
-                    <Th>ID</Th>
-                    <Th>NOME</Th>
-                    <Th>DESCRIÇÃO</Th>
-                    <Th>MARCA</Th>
-                    <Th isNumeric>TENSÃO</Th>
-                    <Th isNumeric>AÇÕES</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td>inches</Td>
-                    <Td>millimetres (mm)</Td>
-                    <Td>25.4</Td>
-                    <Td>25.4</Td>
-                    <Td isNumeric>25.4</Td>
-                    <Td><span className="Acoes-grid"><div id="modal-cadastro"><Modal nome="Alterar"/></div><AlertDialog/></span></Td>
-                  </Tr>
-                  <Tr>
-                    <Td>feet</Td>
-                    <Td>centimetres (cm)</Td>
-                    <Td>30.48</Td>
-                    <Td>25.4</Td>
-                    <Td isNumeric>25.4</Td>
-                    <Td><span className="Acoes-grid"><div id="modal-cadastro"><Modal nome="Alterar"/></div><AlertDialog/></span></Td>
-                  </Tr>
-                  <Tr>
-                    <Td>yards</Td>
-                    <Td>metres (m)</Td>
-                    <Td>0.91444</Td>
-                    <Td>25.4</Td>
-                    <Td isNumeric>25.4</Td>
-                    <Td><span className="Acoes-grid"><div id="modal-cadastro"><Modal nome="Alterar"/></div><AlertDialog/></span></Td>
-                  </Tr>
-                </Tbody>
-                <Tfoot>
-                  <Tr>
-                    <Th >ID</Th>
-                    <Th>NOME</Th>
-                    <Th>DESCRIÇÃO</Th>
-                    <Th >MARCA</Th>
-                    <Th isNumeric>TENSÃO</Th>
-                    <Th isNumeric>AÇÕES</Th>
-                  </Tr>
-                </Tfoot>
-              </Table>
+              <GetRequestProduto/>
             </TableContainer>
           </div>
         </Box>
       </div>  
     </div>
   );
+  
 }
 
 export default App;
-
---
+++ frontend/src/components/requests/GetRequestProduto.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Modal from '../modal/ModalCadastro.js';
+import AlertDialog from '../AlertDialog/AlertDialogComponent.js';
+import {Table,
+        Thead,
+        Tbody,
+        Tfoot,
+        Tr,
+        Th,
+        Td,
+        TableCaption
+  } from '@chakra-ui/react';
+
+class GetRequestProduto extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dadosProduto: []
+        };
+    }
+
+    componentDidMount() {
+        // GET request using fetch with set headers
+        const headers = { 'Content-Type': 'application/json' }
+        fetch('http://localhost:9191/api/search/', { headers })
+            .then(response => response.json())
+            .then(data => this.setState({ dadosProduto: data.data }));
+    }
+
+    render() {
+        const { dadosProduto } = this.state;
+        let rows = [];
+        dadosProduto.forEach((produto, index) => {
+            rows[index] = <Tr key={produto.id}>
+                <Td>{produto.id}</Td>
+                <Td>{produto.nome}</Td>
+                <Td>{produto.descricao}</Td>
+                <Td>{produto.marca_id}</Td>
+                <Td isNumeric>{produto.tensao}</Td>
+                <Td><span className="Acoes-grid"><div id="modal-cadastro"><Modal nome="Alterar"/></div><AlertDialog/></span>
+                </Td>
+                </Tr>;
+        });
+
+        return (
+            <div>
+                <Table variant='striped' colorScheme='teal' size="md">
+                <TableCaption>Produtos de Eletrodomésticos - @author: Washington Monteiro/2023</TableCaption>
+                    <Thead>
+                    <Tr>
+                        <Th>ID</Th>
+                        <Th>NOME</Th>
+                        <Th>DESCRIÇÃO</Th>
+                        <Th>MARCA</Th>
+                        <Th isNumeric>TENSÃO</Th>
+                        <Th isNumeric>AÇÕES</Th>
+                    </Tr>
+                    </Thead>
+                    <Tbody>
+                        {rows}
+                    </Tbody>
+                    <Tfoot>
+                    <Tr>
+                        <Th >ID</Th>
+                        <Th>NOME</Th>
+                        <Th>DESCRIÇÃO</Th>
+                        <Th >MARCA</Th>
+                        <Th isNumeric>TENSÃO</Th>
+                        <Th isNumeric>AÇÕES</Th>
+                    </Tr>
+                    </Tfoot>
+                </Table>
+            </div>
+        );
+    }
+}
+
+export { GetRequestProduto }; 
