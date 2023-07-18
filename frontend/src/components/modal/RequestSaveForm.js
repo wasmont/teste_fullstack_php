a29464856b6358e0d8039c7f5906c69265e47f30
washington.monteiro <washington.monteiro@executiva.com.br>
2023-07-18 15:43:31 -0300

    Adição de Registro Implementado

--- frontend/src/components/modal/RequestSaveForm.js
+++ frontend/src/components/modal/RequestSaveForm.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 export default function RequestSaveForm({parentToForm}) { 
   
@@ -12,14 +18,36 @@ export default function RequestSaveForm({parentToForm}) {
     const [data, setData] = useState([]);
     const [error, setError] = useState("");
 
-  if(parentToForm || parentToForm.nome.lenght >0) {
-
+  if(parentToForm && (parentToForm.nome !== undefined)) {
+    
       axios.post("http://localhost:9191/api/create",payload)
         .then((response) => setData(response.data.data))
         .catch((err) => {
-          setError("ops! ocorreu um erro" + err);
+          setError(err.message);
         });
+
   }      
-  //console.log(JSON.stringify(error));
-  return <></>
+
+  if((data.id === undefined) && (error === undefined)) return null;
+
+  function reloadPage() {
+    setTimeout(function(){
+        window.location.reload();
+    }, 3000);
+  };
+
+  return <>
+          {data && (data.id !== undefined) ? (
+            <Alert status='success'>
+              <AlertIcon />
+              <AlertTitle>Adicionado!</AlertTitle>
+              <AlertDescription>Um novo registro foi adicionado.{reloadPage()}</AlertDescription>
+            </Alert>) :
+            ( error || (error !== undefined) || error.length > 0 ? (
+            <Alert status='error'>
+              <AlertIcon />
+              <AlertTitle>Erro!</AlertTitle>
+              <AlertDescription>Não foi possível adicionar o eletrodoméstico!</AlertDescription>
+            </Alert>): (null) )}
+        </>
 }
